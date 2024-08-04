d9bac4fa225120bd4be7ca299cab8bf104d6b7ab
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-08-04 22:57:47 +0530

    added waller

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,21 @@
-export default function() {
-    return  <div>
-        Dashboard
+//Learn about server components --> where we can access our db and other server component
+
+import { getServerSession } from "next-auth";
+import db from '@/app/db'
+import ProfileCard from "../components/ProfileCard";
+import { authConfig } from "../lib/auth";
+
+async function getBalance() {
+    const session = await getServerSession(authConfig)
+    db.solWallet.findFirst({
+        where: {
+            userId: session?.user?.uid
+        }
+    })
+}
+
+export default async function() {
+    return <div>
+        <ProfileCard />
     </div>
 }
